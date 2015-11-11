405b352aff3d9dd1ade973a057af07f5e2f7cc7e
denissellu <hingasellu@gmail.com>
2015-11-11 22:59:18 +0000

    Call google only when route boxer is loaded

--- src/RouteBoxer.js
+++ src/RouteBoxer.js
@@ -29,7 +29,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 /**
@@ -39,6 +39,7 @@
  */
 function RouteBoxer() {
   this.R = 6371; // earth's mean radius in km
+  this.initGoogle();
 }
 
 /**
@@ -56,13 +57,13 @@ function RouteBoxer() {
 RouteBoxer.prototype.box = function (path, range) {
   // Two dimensional array representing the cells in the grid overlaid on the path
   this.grid_ = null;
-  
+
   // Array that holds the latitude coordinate of each vertical grid line
   this.latGrid_ = [];
-  
-  // Array that holds the longitude coordinate of each horizontal grid line  
+
+  // Array that holds the longitude coordinate of each horizontal grid line
   this.lngGrid_ = [];
-  
+
   // Array of bounds that cover the whole route formed by merging cells that
   //  the route intersects first horizontally, and then vertically
   this.boxesX_ = [];
@@ -70,7 +71,7 @@ RouteBoxer.prototype.box = function (path, range) {
   // Array of bounds that cover the whole route formed by merging cells that
   //  the route intersects first vertically, and then horizontally
   this.boxesY_ = [];
-  
+
   // The array of LatLngs representing the vertices of the path
   var vertices = null;
 
@@ -96,10 +97,10 @@ RouteBoxer.prototype.box = function (path, range) {
 
   // Build the grid that is overlaid on the route
   this.buildGrid_(vertices, range);
-  
+
   // Identify the grid cells that the route intersects
   this.findIntersectingCells_(vertices);
-  
+
   // Merge adjacent intersected grid cells (and their neighbours) into two sets
   //  of bounds, both of which cover them completely
   this.mergeIntersectingCells_();
@@ -123,40 +124,40 @@ RouteBoxer.prototype.buildGrid_ = function (vertices, range) {
   for (var i = 0; i < vertices.length; i++) {
     routeBounds.extend(vertices[i]);
   }
-  
+
   // Find the center of the bounding box of the path
   var routeBoundsCenter = routeBounds.getCenter();
-  
+
   // Starting from the center define grid lines outwards vertically until they
   //  extend beyond the edge of the bounding box by more than one cell
   this.latGrid_.push(routeBoundsCenter.lat());
-  
+
   // Add lines from the center out to the north
   this.latGrid_.push(routeBoundsCenter.rhumbDestinationPoint(0, range).lat());
   for (i = 2; this.latGrid_[i - 2] < routeBounds.getNorthEast().lat(); i++) {
     this.latGrid_.push(routeBoundsCenter.rhumbDestinationPoint(0, range * i).lat());
   }
 
-  // Add lines from the center out to the south  
+  // Add lines from the center out to the south
   for (i = 1; this.latGrid_[1] > routeBounds.getSouthWest().lat(); i++) {
     this.latGrid_.unshift(routeBoundsCenter.rhumbDestinationPoint(180, range * i).lat());
   }
 
   // Starting from the center define grid lines outwards horizontally until they
-  //  extend beyond the edge of the bounding box by more than one cell  
+  //  extend beyond the edge of the bounding box by more than one cell
   this.lngGrid_.push(routeBoundsCenter.lng());
-  
+
   // Add lines from the center out to the east
   this.lngGrid_.push(routeBoundsCenter.rhumbDestinationPoint(90, range).lng());
   for (i = 2; this.lngGrid_[i - 2] < routeBounds.getNorthEast().lng(); i++) {
     this.lngGrid_.push(routeBoundsCenter.rhumbDestinationPoint(90, range * i).lng());
   }
-  
+
   // Add lines from the center out to the west
   for (i = 1; this.lngGrid_[1] > routeBounds.getSouthWest().lng(); i++) {
     this.lngGrid_.unshift(routeBoundsCenter.rhumbDestinationPoint(270, range * i).lng());
   }
-  
+
   // Create a two dimensional array representing this grid
   this.grid_ = new Array(this.lngGrid_.length);
   for (i = 0; i < this.grid_.length; i++) {
@@ -172,7 +173,7 @@ RouteBoxer.prototype.buildGrid_ = function (vertices, range) {
 RouteBoxer.prototype.findIntersectingCells_ = function (vertices) {
   // Find the cell where the path begins
   var hintXY = this.getCellCoords_(vertices[0]);
-  
+
   // Mark that cell and it's neighbours for inclusion in the boxes
   this.markCell_(hintXY);
 
@@ -180,18 +181,18 @@ RouteBoxer.prototype.findIntersectingCells_ = function (vertices) {
   for (var i = 1; i < vertices.length; i++) {
     // Use the known cell of the previous vertex to help find the cell of this vertex
     var gridXY = this.getGridCoordsFromHint_(vertices[i], vertices[i - 1], hintXY);
-    
+
     if (gridXY[0] === hintXY[0] && gridXY[1] === hintXY[1]) {
       // This vertex is in the same cell as the previous vertex
       // The cell will already have been marked for inclusion in the boxes
       continue;
-    
+
     } else if ((Math.abs(hintXY[0] - gridXY[0]) === 1 && hintXY[1] === gridXY[1]) ||
         (hintXY[0] === gridXY[0] && Math.abs(hintXY[1] - gridXY[1]) === 1)) {
       // This vertex is in a cell that shares an edge with the previous cell
       // Mark this cell and it's neighbours for inclusion in the boxes
       this.markCell_(gridXY);
-      
+
     } else {
       // This vertex is in a cell that does not share an edge with the previous
       //  cell. This means that the path passes through other cells between
@@ -199,7 +200,7 @@ RouteBoxer.prototype.findIntersectingCells_ = function (vertices) {
       //  it passes through
       this.getGridIntersects_(vertices[i - 1], vertices[i], hintXY, gridXY);
     }
-    
+
     // Use this cell to find and compare with the next one
     hintXY = gridXY;
   }
@@ -210,7 +211,7 @@ RouteBoxer.prototype.findIntersectingCells_ = function (vertices) {
  *
  * @param {LatLng[]} latlng The latlng of the vertex
  * @return {Number[][]} The cell coordinates of this vertex in the grid
- */ 
+ */
 RouteBoxer.prototype.getCellCoords_ = function (latlng) {
   for (var x = 0; this.lngGrid_[x] < latlng.lng(); x++) {}
   for (var y = 0; this.latGrid_[y] < latlng.lat(); y++) {}
@@ -226,7 +227,7 @@ RouteBoxer.prototype.getCellCoords_ = function (latlng) {
  * @param {LatLng[]} hintlatlng The latlng of the vertex with a known location
  * @param {Number[]} hint The cell containing the vertex with a known location
  * @return {Number[]} The cell coordinates of the vertex to locate in the grid
- */ 
+ */
 RouteBoxer.prototype.getGridCoordsFromHint_ = function (latlng, hintlatlng, hint) {
   var x, y;
   if (latlng.lng() > hintlatlng.lng()) {
@@ -234,13 +235,13 @@ RouteBoxer.prototype.getGridCoordsFromHint_ = function (latlng, hintlatlng, hint
   } else {
     for (x = hint[0]; this.lngGrid_[x] > latlng.lng(); x--) {}
   }
-  
+
   if (latlng.lat() > hintlatlng.lat()) {
     for (y = hint[1]; this.latGrid_[y + 1] < latlng.lat(); y++) {}
-  } else {        
+  } else {
     for (y = hint[1]; this.latGrid_[y] > latlng.lat(); y--) {}
   }
-  
+
   return ([x, y]);
 };
 
@@ -257,19 +258,19 @@ RouteBoxer.prototype.getGridCoordsFromHint_ = function (latlng, hintlatlng, hint
  * 5. Filling in all squares between the x-coord of the previous intersection
  *     (or start) and the current one (or end) at the current y coordinate,
  *     which is known for the grid line being intersected
- *     
+ *
  * @param {LatLng} start The latlng of the vertex at the start of the segment
  * @param {LatLng} end The latlng of the vertex at the end of the segment
  * @param {Number[]} startXY The cell containing the start vertex
  * @param {Number[]} endXY The cell containing the vend vertex
- */ 
+ */
 RouteBoxer.prototype.getGridIntersects_ = function (start, end, startXY, endXY) {
   var edgePoint, edgeXY, i;
   var brng = start.rhumbBearingTo(end);         // Step 1.
-  
+
   var hint = start;
   var hintXY = startXY;
-  
+
   // Handle a line segment that travels south first
   if (end.lat() > start.lat()) {
     // Iterate over the east to west grid lines between the start and end cells
@@ -277,31 +278,31 @@ RouteBoxer.prototype.getGridIntersects_ = function (start, end, startXY, endXY) 
       // Find the latlng of the point where the path segment intersects with
       //  this grid line (Step 2 & 3)
       edgePoint = this.getGridIntersect_(start, brng, this.latGrid_[i]);
-      
+
       // Find the cell containing this intersect point (Step 4)
       edgeXY = this.getGridCoordsFromHint_(edgePoint, hint, hintXY);
-      
+
       // Mark every cell the path has crossed between this grid and the start,
       //   or the previous east to west grid line it crossed (Step 5)
       this.fillInGridSquares_(hintXY[0], edgeXY[0], i - 1);
-      
+
       // Use the point where it crossed this grid line as the reference for the
       //  next iteration
       hint = edgePoint;
       hintXY = edgeXY;
     }
-    
+
     // Mark every cell the path has crossed between the last east to west grid
     //  line it crossed and the end (Step 5)
     this.fillInGridSquares_(hintXY[0], endXY[0], i - 1);
-    
+
   } else {
     // Iterate over the east to west grid lines between the start and end cells
     for (i = startXY[1]; i > endXY[1]; i--) {
       // Find the latlng of the point where the path segment intersects with
       //  this grid line (Step 2 & 3)
       edgePoint = this.getGridIntersect_(start, brng, this.latGrid_[i]);
-      
+
       // Find the cell containing this intersect point (Step 4)
       edgeXY = this.getGridCoordsFromHint_(edgePoint, hint, hintXY);
 
@@ -314,24 +315,24 @@ RouteBoxer.prototype.getGridIntersects_ = function (start, end, startXY, endXY) 
       hint = edgePoint;
       hintXY = edgeXY;
     }
-    
+
     // Mark every cell the path has crossed between the last east to west grid
     //  line it crossed and the end (Step 5)
     this.fillInGridSquares_(hintXY[0], endXY[0], i);
-    
+
   }
 };
 
 /**
  * Find the latlng at which a path segment intersects with a given
  *   line of latitude
- *     
+ *
  * @param {LatLng} start The vertex at the start of the path segment
  * @param {Number} brng The bearing of the line from start to end
  * @param {Number} gridLineLat The latitude of the grid line being intersected
  * @return {LatLng} The latlng of the point where the path segment intersects
  *                    the grid line
- */ 
+ */
 RouteBoxer.prototype.getGridIntersect_ = function (start, brng, gridLineLat) {
   var d = this.R * ((gridLineLat.toRad() - start.lat().toRad()) / Math.cos(brng.toRad()));
   return start.rhumbDestinationPoint(brng, d);
@@ -340,11 +341,11 @@ RouteBoxer.prototype.getGridIntersect_ = function (start, brng, gridLineLat) {
 /**
  * Mark all cells in a given row of the grid that lie between two columns
  *   for inclusion in the boxes
- *     
+ *
  * @param {Number} startx The first column to include
  * @param {Number} endx The last column to include
  * @param {Number} y The row of the cells to include
- */ 
+ */
 RouteBoxer.prototype.fillInGridSquares_ = function (startx, endx, y) {
   var x;
   if (startx < endx) {
@@ -354,15 +355,15 @@ RouteBoxer.prototype.fillInGridSquares_ = function (startx, endx, y) {
   } else {
     for (x = startx; x >= endx; x--) {
       this.markCell_([x, y]);
-    }            
-  }      
+    }
+  }
 };
 
 /**
  * Mark a cell and the 8 immediate neighbours for inclusion in the boxes
- *     
+ *
  * @param {Number[]} square The cell to mark
- */ 
+ */
 RouteBoxer.prototype.markCell_ = function (cell) {
   var x = cell[0];
   var y = cell[1];
@@ -388,18 +389,18 @@ RouteBoxer.prototype.markCell_ = function (cell) {
  * The second set is created by combining adjacent cells in the same row into
  *   a set of horizontal rectangular boxes, and then combining boxes of the same
  *   width that are adjacent vertically.
- *     
- */ 
+ *
+ */
 RouteBoxer.prototype.mergeIntersectingCells_ = function () {
   var x, y, box;
-  
+
   // The box we are currently expanding with new cells
   var currentBox = null;
-  
+
   // Traverse the grid a row at a time
   for (y = 0; y < this.grid_[0].length; y++) {
     for (x = 0; x < this.grid_.length; x++) {
-      
+
       if (this.grid_[x][y]) {
         // This cell is marked for inclusion. If the previous cell in this
         //   row was also marked for inclusion, merge this cell into it's box.
@@ -410,7 +411,7 @@ RouteBoxer.prototype.mergeIntersectingCells_ = function () {
         } else {
           currentBox = box;
         }
-        
+
       } else {
         // This cell is not marked for inclusion. If the previous cell was
         //  marked for inclusion, merge it's box with a box that spans the same
@@ -429,7 +430,7 @@ RouteBoxer.prototype.mergeIntersectingCells_ = function () {
   for (x = 0; x < this.grid_.length; x++) {
     for (y = 0; y < this.grid_[0].length; y++) {
       if (this.grid_[x][y]) {
-        
+
         // This cell is marked for inclusion. If the previous cell in this
         //   column was also marked for inclusion, merge this cell into it's box.
         // Otherwise start a new box.
@@ -439,14 +440,14 @@ RouteBoxer.prototype.mergeIntersectingCells_ = function () {
         } else {
           currentBox = this.getCellBounds_([x, y]);
         }
-        
+
       } else {
         // This cell is not marked for inclusion. If the previous cell was
         //  marked for inclusion, merge it's box with a box that spans the same
         //  rows from the column to the left if possible.
         this.mergeBoxesX_(currentBox);
         currentBox = null;
-        
+
       }
     }
     // If the last cell was marked for inclusion, merge it's box with a matching
@@ -462,7 +463,7 @@ RouteBoxer.prototype.mergeIntersectingCells_ = function () {
  * is not found, append this box to the list of existing boxes.
  *
  * @param {LatLngBounds}  The box to merge
- */ 
+ */
 RouteBoxer.prototype.mergeBoxesX_ = function (box) {
   if (box !== null) {
     for (var i = 0; i < this.boxesX_.length; i++) {
@@ -483,7 +484,7 @@ RouteBoxer.prototype.mergeBoxesX_ = function (box) {
  * is not found, append this box to the list of existing boxes.
  *
  * @param {LatLngBounds}  The box to merge
- */ 
+ */
 RouteBoxer.prototype.mergeBoxesY_ = function (box) {
   if (box !== null) {
     for (var i = 0; i < this.boxesY_.length; i++) {
@@ -503,47 +504,51 @@ RouteBoxer.prototype.mergeBoxesY_ = function (box) {
  *
  * @param {Number[]} cell The cell to lookup.
  * @return {LatLng} The latlng of the origin of the cell.
- */ 
+ */
 RouteBoxer.prototype.getCellBounds_ = function (cell) {
   return new google.maps.LatLngBounds(
     new google.maps.LatLng(this.latGrid_[cell[1]], this.lngGrid_[cell[0]]),
     new google.maps.LatLng(this.latGrid_[cell[1] + 1], this.lngGrid_[cell[0] + 1]));
 };
 
-/* Based on the Latitude/longitude spherical geodesy formulae & scripts
-   at http://www.movable-type.co.uk/scripts/latlong.html
-   (c) Chris Veness 2002-2010
-*/ 
-google.maps.LatLng.prototype.rhumbDestinationPoint = function (brng, dist) {
-  var R = 6371; // earth's mean radius in km
-  var d = parseFloat(dist) / R;  // d = angular distance covered on earth's surface
-  var lat1 = this.lat().toRad(), lon1 = this.lng().toRad();
-  brng = brng.toRad();
 
-  var lat2 = lat1 + d * Math.cos(brng);
-  var dLat = lat2 - lat1;
-  var dPhi = Math.log(Math.tan(lat2 / 2 + Math.PI / 4) / Math.tan(lat1 / 2 + Math.PI / 4));
-  var q = (Math.abs(dLat) > 1e-10) ? dLat / dPhi : Math.cos(lat1);
-  var dLon = d * Math.sin(brng) / q;
-  // check for going past the pole
-  if (Math.abs(lat2) > Math.PI / 2) {
-    lat2 = lat2 > 0 ? Math.PI - lat2 : - (Math.PI - lat2);
-  }
-  var lon2 = (lon1 + dLon + Math.PI) % (2 * Math.PI) - Math.PI;
- 
-  if (isNaN(lat2) || isNaN(lon2)) {
-    return null;
-  }
-  return new google.maps.LatLng(lat2.toDeg(), lon2.toDeg());
-};
+RouteBoxer.prototype.initGoogle = function() {
+  /* Based on the Latitude/longitude spherical geodesy formulae & scripts
+     at http://www.movable-type.co.uk/scripts/latlong.html
+     (c) Chris Veness 2002-2010
+     */
+  google.maps.LatLng.prototype.rhumbDestinationPoint = function (brng, dist) {
+    var R = 6371; // earth's mean radius in km
+    var d = parseFloat(dist) / R;  // d = angular distance covered on earth's surface
+    var lat1 = this.lat().toRad(), lon1 = this.lng().toRad();
+    brng = brng.toRad();
 
-google.maps.LatLng.prototype.rhumbBearingTo = function (dest) {
-  var dLon = (dest.lng() - this.lng()).toRad();
-  var dPhi = Math.log(Math.tan(dest.lat().toRad() / 2 + Math.PI / 4) / Math.tan(this.lat().toRad() / 2 + Math.PI / 4));
-  if (Math.abs(dLon) > Math.PI) {
-    dLon = dLon > 0 ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
-  }
-  return Math.atan2(dLon, dPhi).toBrng();
+    var lat2 = lat1 + d * Math.cos(brng);
+    var dLat = lat2 - lat1;
+    var dPhi = Math.log(Math.tan(lat2 / 2 + Math.PI / 4) / Math.tan(lat1 / 2 + Math.PI / 4));
+    var q = (Math.abs(dLat) > 1e-10) ? dLat / dPhi : Math.cos(lat1);
+    var dLon = d * Math.sin(brng) / q;
+    // check for going past the pole
+    if (Math.abs(lat2) > Math.PI / 2) {
+      lat2 = lat2 > 0 ? Math.PI - lat2 : - (Math.PI - lat2);
+    }
+    var lon2 = (lon1 + dLon + Math.PI) % (2 * Math.PI) - Math.PI;
+
+    if (isNaN(lat2) || isNaN(lon2)) {
+      return null;
+    }
+    return new google.maps.LatLng(lat2.toDeg(), lon2.toDeg());
+  };
+
+  google.maps.LatLng.prototype.rhumbBearingTo = function (dest) {
+    var dLon = (dest.lng() - this.lng()).toRad();
+    var dPhi = Math.log(Math.tan(dest.lat().toRad() / 2 + Math.PI / 4) / Math.tan(this.lat().toRad() / 2 + Math.PI / 4));
+    if (Math.abs(dLon) > Math.PI) {
+      dLon = dLon > 0 ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
+    }
+    return Math.atan2(dLon, dPhi).toBrng();
+  };
+
 };
 
 /**
@@ -551,7 +556,7 @@ google.maps.LatLng.prototype.rhumbBearingTo = function (dest) {
  *
  * @return {Number} Bearing in radians
  * @ignore
- */ 
+ */
 Number.prototype.toRad = function () {
   return this * Math.PI / 180;
 };
@@ -561,7 +566,7 @@ Number.prototype.toRad = function () {
  *
  * @return {Number} Bearing in degrees
  * @ignore
- */ 
+ */
 Number.prototype.toDeg = function () {
   return this * 180 / Math.PI;
 };
@@ -569,9 +574,9 @@ Number.prototype.toDeg = function () {
 /**
  * Normalize a heading in degrees to between 0 and +360
  *
- * @return {Number} Return 
+ * @return {Number} Return
  * @ignore
- */ 
+ */
 Number.prototype.toBrng = function () {
   return (this.toDeg() + 360) % 360;
 };
